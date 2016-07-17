2a666ec6645ce3122728fcc71617831f93ac6548
muchigo <safwan811@gmail.com>
2016-07-18 05:14:24 +0800

    Update Debian-kvm.sh

--- Debian-kvm.sh
+++ Debian-kvm.sh
@@ -146,6 +146,7 @@ wget "http://prdownloads.sourceforge.net/webadmin/webmin_1.801_all.deb"
 dpkg --install webmin_1.801_all.deb;
 apt-get -y -f install;
 rm /root/webmin_1.801_all.deb
+sed -i 's/ssl=1/ssl=0/g' /etc/webmin/miniserv.conf
 service webmin restart
 service vnstat restart
 
