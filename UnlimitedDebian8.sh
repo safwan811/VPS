f39e4cf7471e5d326f3ebc39f23700c3dbe438e3
muchigo <safwan811@gmail.com>
2016-07-20 03:15:07 +0800

    Update UnlimitedDebian8.sh

--- UnlimitedDebian8.sh
+++ UnlimitedDebian8.sh
@@ -37,7 +37,7 @@ rm /etc/nginx/sites-enabled/default
 rm /etc/nginx/sites-available/default
 wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/muchigo/VPS/master/conf/nginx.conf"
 mkdir -p /home/vps/public_html
-echo "<pre>Setup by Muchigo</pre>" > /home/vps/public_html/index.html
+echo "<pre>Setup by Kiellez</pre>" > /home/vps/public_html/index.html
 echo "<?php phpinfo(); ?>" > /home/vps/public_html/info.php
 wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/muchigo/VPS/master/conf/vps.conf"
 sed -i 's/listen = \/var\/run\/php5-fpm.sock/listen = 127.0.0.1:9000/g' /etc/php5/fpm/pool.d/www.conf
