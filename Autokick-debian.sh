fe4c7d323eec85c4962d491c56e2fe7ead03231e
muchigo <safwan811@gmail.com>
2016-08-07 18:41:10 +0800

    Update Autokick-debian.sh

--- Autokick-debian.sh
+++ Autokick-debian.sh
@@ -8,17 +8,17 @@ wget https://raw.githubusercontent.com/muchigo/VPS/master/conf/userlimit-debian.
 chmod +x userlimit-debian.sh
 
 # setup cron for userlimit
-echo "* * * * * root /root/userlimit.sh" >> /etc/crontab
-echo "* * * * * root sleep 5; /root/userlimit.sh" >> /etc/crontab
-echo "* * * * * root sleep 10; /root/userlimit.sh" >> /etc/crontab
-echo "* * * * * root sleep 15; /root/userlimit.sh" >> /etc/crontab
-echo "* * * * * root sleep 20; /root/userlimit.sh" >> /etc/crontab
-echo "* * * * * root sleep 25; /root/userlimit.sh" >> /etc/crontab
-echo "* * * * * root sleep 30; /root/userlimit.sh" >> /etc/crontab
-echo "* * * * * root sleep 35; /root/userlimit.sh" >> /etc/crontab
-echo "* * * * * root sleep 40; /root/userlimit.sh" >> /etc/crontab
-echo "* * * * * root sleep 45; /root/userlimit.sh" >> /etc/crontab
-echo "* * * * * root sleep 50; /root/userlimit.sh" >> /etc/crontab
-echo "* * * * * root sleep 55; /root/userlimit.sh" >> /etc/crontab
+echo "* * * * * root /root/userlimit-debian.sh" >> /etc/crontab
+echo "* * * * * root sleep 5; /root/userlimit-debian.sh" >> /etc/crontab
+echo "* * * * * root sleep 10; /root/userlimit-debian.sh" >> /etc/crontab
+echo "* * * * * root sleep 15; /root/userlimit-debian.sh" >> /etc/crontab
+echo "* * * * * root sleep 20; /root/userlimit-debian.sh" >> /etc/crontab
+echo "* * * * * root sleep 25; /root/userlimit-debian.sh" >> /etc/crontab
+echo "* * * * * root sleep 30; /root/userlimit-debian.sh" >> /etc/crontab
+echo "* * * * * root sleep 35; /root/userlimit-debian.sh" >> /etc/crontab
+echo "* * * * * root sleep 40; /root/userlimit-debian.sh" >> /etc/crontab
+echo "* * * * * root sleep 45; /root/userlimit-debian.sh" >> /etc/crontab
+echo "* * * * * root sleep 50; /root/userlimit-debian.sh" >> /etc/crontab
+echo "* * * * * root sleep 55; /root/userlimit-debian.sh" >> /etc/crontab
 
-service crond restart
+service cron restart
