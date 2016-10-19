da9058ead833a9e30e03e196eb01c6bb016da666
muchigo <safwan811@gmail.com>
2016-10-19 08:06:44 +0800

    Update Debian-kvm.sh

--- Debian-kvm.sh
+++ Debian-kvm.sh
@@ -140,10 +140,10 @@ service squid3 restart
 
 # install webmin
 cd
-wget "http://prdownloads.sourceforge.net/webadmin/webmin_1.801_all.deb"
-dpkg --install webmin_1.801_all.deb;
+wget "http://prdownloads.sourceforge.net/webadmin/webmin_1.820_all.deb"
+dpkg --install webmin_1.820_all.deb;
 apt-get -y -f install;
-rm /root/webmin_1.801_all.deb
+rm /root/webmin_1.820_all.deb
 sed -i 's/ssl=1/ssl=0/g' /etc/webmin/miniserv.conf
 service webmin restart
 service vnstat restart
